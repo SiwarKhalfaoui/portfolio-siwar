60c686decd8144263621ffcb600b3a42389db320
siwar.khalfaoui <siouarkhalfaoui@gmail.com>
2026-03-23 01:15:23 +0100

    feat: add reusable UI components (navbar, timeline, modals, animations, cards)

---
+++ src/components/floating-navbar.tsx
@@ -0,0 +1,144 @@
+"use client";
+
+import React, { JSX, useEffect, useState } from "react";
+import {
+  motion,
+  AnimatePresence,
+  useScroll,
+  useMotionValueEvent,
+} from "framer-motion";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useTheme } from "next-themes";
+import { Sun, Moon } from "lucide-react";
+import { IconHome, IconMessage, IconBrain } from "@tabler/icons-react";
+import { BriefcaseBusiness, FolderOpen, GraduationCap } from "lucide-react";
+
+const navItems = [
+  {
+    name: "Home",
+    link: "#home",
+    icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+    icon: <IconBrain className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Career",
+    link: "#career",
+    icon: (
+      <BriefcaseBusiness className="h-4 w-4 text-neutral-500 dark:text-white" />
+    ),
+  },
+  {
+    name: "Projects",
+    link: "#projects",
+    icon: <FolderOpen className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Education",
+    link: "#education",
+    icon: (
+      <GraduationCap className="h-4 w-4 text-neutral-500 dark:text-white" />
+    ),
+  },
+  {
+    name: "Contact",
+    link: "#contact",
+    icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+];
+
+interface NavItem {
+  name: string;
+  link: string;
+  icon?: JSX.Element;
+}
+
+export const FloatingNav = ({ className }: { className?: string }) => {
+  const { theme, setTheme } = useTheme();
+
+  const [mounted, setMounted] = useState(false);
+
+  // Prevent hydration mismatch
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const { scrollYProgress } = useScroll();
+
+  const [visible, setVisible] = useState(true);
+
+  useMotionValueEvent(scrollYProgress, "change", (current) => {
+    // Check if current is not undefined and is a number
+    if (typeof current === "number") {
+      const direction = current! - scrollYProgress.getPrevious()!;
+
+      if (scrollYProgress.get() < 0) {
+        setVisible(false);
+      } else {
+        if (direction < 0) {
+          setVisible(true);
+        } else {
+          setVisible(false);
+        }
+      }
+    }
+  });
+
+  if (!mounted) return null; // Ensure correct theme renders
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        initial={{
+          opacity: 1,
+          y: -100,
+        }}
+        animate={{
+          y: visible ? 0 : -100,
+          opacity: visible ? 1 : 0,
+        }}
+        transition={{
+          duration: 0.2,
+        }}
+        className={cn(
+          "flex max-w-fit  fixed top-2 sm:top-5 lg:top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[49] pr-2 pl-8 py-2  items-center justify-center space-x-4",
+          className
+        )}
+      >
+        {navItems.map((navItem: NavItem, idx: number) => (
+          <Link
+            key={`link=${idx}`}
+            href={navItem.link}
+            className={cn(
+              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+            )}
+          >
+            <span className="block sm:hidden">{navItem.icon}</span>
+            <span className="hidden sm:block text-sm">{navItem.name}</span>
+          </Link>
+        ))}
+        <button
+          className="relative w-[2rem] h-[2rem] border text-sm font-medium border-neutral-200 dark:border-white/[0.2] text-black dark:text-white rounded-full flex items-center justify-center"
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+          <div className="relative w-[1.2rem] h-[1.2rem]">
+            <Sun
+              className={`absolute w-[1.2rem] h-[1.2rem] inset-0 transition-all duration-500
+        ${theme === "light" ? "rotate-0 scale-100" : "-rotate-90 scale-0"}`}
+            />
+            <Moon
+              className={`absolute w-[1.2rem] h-[1.2rem] inset-0 transition-all duration-500
+        ${theme === "dark" ? "rotate-0 scale-100" : "rotate-90 scale-0"}`}
+            />
+          </div>
+
+          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
+        </button>
+      </motion.div>
+    </AnimatePresence>
+  );
+};
