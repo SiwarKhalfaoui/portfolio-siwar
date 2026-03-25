a86c2526fee5fb7abd56451bf34f3c5a116fb616
siwar.khalfaoui <siouarkhalfaoui@gmail.com>
2026-03-25 12:10:42 +0100

    Update title

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -18,10 +18,10 @@ export default function Hero() {
   Full Stack Developer <br />
   <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
     <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent py-4 bg-gradient-to-r from-violet-500 via-fuchsia-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-      <span>Software Engineering Student</span>
+      <span>Engineering Student</span>
     </div>
     <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-violet-500 via-fuchsia-500 to-pink-500 py-4">
-      <span>Software Engineering Student</span>
+      <span>Engineering Student</span>
     </div>
   </div>
 </h2>
