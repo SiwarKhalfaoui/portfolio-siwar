3292eb70a487329cd95bba7413dfefb34016ff05
siwar.khalfaoui <siouarkhalfaoui@gmail.com>
2026-03-25 11:45:49 +0100

    msg

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -15,15 +15,7 @@ export default function Hero() {
     >
       {/* Left — Title */}
       <h2 className="relative z-20 text-3xl md:text-5xl xl:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
-        Full Stack Developer <br />
-        <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
-          <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent py-4 bg-gradient-to-r from-violet-500 via-fuchsia-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-            <span>3rd year at ESPRIT</span>
-          </div>
-          <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-violet-500 via-fuchsia-500 to-pink-500 py-4">
-            <span>3rd year at ESPRIT</span>
-          </div>
-        </div>
+       Full Stack Developer
       </h2>
 
       {/* Right — Photo + Name + Buttons */}
